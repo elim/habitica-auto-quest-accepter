dd21fe52e574bc8a649468c0cd72017d0dc17e20
Takeru Naito <takeru.naito@gmail.com>
2018-08-08 22:57:52 +0900

    Use the single quote if possible

--- accepter.sh
+++ accepter.sh
@@ -15,28 +15,28 @@ quest_data=$(
 
 quest_active=$(echo "${quest_data}" | jq '.active')
 
-if [ "${quest_active}" = "true" ]; then
+if [ "${quest_active}" = 'true' ]; then
   quest_key=$(echo "${quest_data}" | jq '.key')
   echo "The quest ${quest_key} has already started."
   exit
 fi
 
-if [ "$(echo "${quest_data}" | jq ".members")" = "{}" ]; then
-  echo "There was no invitation to a quest."
+if [ "$(echo "${quest_data}" | jq '.members')" = '{}' ]; then
+  echo 'There was no invitation to a quest.'
   exit
 fi
 
 is_accepted=$(echo "${quest_data}" | jq ".members[\"${api_user}\"]")
 
-if [ "${is_accepted}" = "true" ]; then
-  echo "You have already accepted it."
+if [ "${is_accepted}" = 'true' ]; then
+  echo 'You have already accepted it.'
   exit
 fi
 
 curl \
   --silent \
   --request POST \
-  --header "Content-Length: 0" \
+  --header 'Content-Length: 0' \
   --header "x-api-key: ${api_key}" \
   --header "x-api-user: ${api_user}" \
   "${API_V3_URL}/groups/party/quests/accept"
